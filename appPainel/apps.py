3d155eb4145daf7a5c3e15948f3ec69d6ef603e4
Hugo Medeiros <hugoavmedeiros@gmail.com>
2023-09-06 12:59:01 -0300

    atualização de nomes no template base do jazzmin

--- appPainel/apps.py
+++ appPainel/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ApppainelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'appPainel'
+    verbose_name = 'Formulários'
